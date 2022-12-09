b70aa86886cc8a955a2a59a79aa76952d9cb55d8
Moein Madadi <moeinmadadi2002@gmail.com>
2022-12-09 16:54:32 +0330

    add home page and start ticket-list

---
+++ pages/home/home.js
@@ -0,0 +1,156 @@
+incPsgBtn = document.getElementById('inc-psg-btn');
+dcrPsgBtn = document.getElementById('dcr-psg-btn');
+psgNumber = document.getElementById('psg-input');
+
+psgNumber.addEventListener("input", function (e) {
+    if (e.target.value < 1) {
+        psgNumber.value = 1;
+    }
+})
+
+incPsgBtn.addEventListener("click", function (e) {
+    psgNumber.value = parseInt(psgNumber.value) + 1;
+    e.preventDefault();
+})
+
+dcrPsgBtn.addEventListener("click", function (e) {
+    psgNumber.value = parseInt(psgNumber.value) - 1;
+    if (psgNumber.value < 1) {
+        psgNumber.value = 1;
+    }
+    e.preventDefault();
+})
+
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+var airports = ["همدان", "یزد", "کیش", "بین المللی امام خمینی(تهران)", "بین المللی مهرآباد(تهران)", "مشهد", "کرمان"];
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+originAirport = document.getElementById("origin-airport");
+destinationAirport = document.getElementById("destination-airport");
+departTime = document.getElementById("depart-time");
+returnTime = document.getElementById("return-time");
+searchTicketBtn = document.getElementById("search-ticket-btn");
+
+autocomplete(originAirport, airports);
+autocomplete(destinationAirport, airports);
+
+searchTicketBtn.addEventListener("click", function () {
+    airplaneTicketSearch = {
+        ticketId: uuid.v4(),
+        userId: "",
+        originAirportName: originAirport.value,
+        destinationAirportName: destinationAirport.value,
+        departTime: departTime.value,
+        returnTime: returnTime.value
+    }
+
+    sessionStorage.setItem(ticketId, airplaneTicketSearch);
+})
+
