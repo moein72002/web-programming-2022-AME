12fbde28da0a7c9343298d6f05d982c67dca82e4
AmirMohammad.afl <a.aflatoonian@tapsell.ir>
2022-12-10 18:16:25 +0330

    add buy-ticket.js

---
+++ pages/buy-ticket/buy-ticket.js
@@ -0,0 +1,32 @@
+const numberOfPassengers = 5;
+for (let index = 1; index <= numberOfPassengers; index++) {
+    const html = 
+`
+<div class="indexNumber">
+${index}
+</div>
+
+<div class="inputBox">
+
+<span>نام :</span>
+<input type="text" placeholder="نام">
+</div>  
+
+<div class="inputBox">
+<span>نام خانوادگی :</span>
+<input type="text" placeholder="نام خانوادگی">
+</div>
+
+<div class="inputBox">
+<span>شماره پاسپورت :</span>
+<input type="number" placeholder="شماره پاسپورت">
+</div>
+<hr size="8" width="100%" color="green" align="center" >
+`
+    document.getElementById('formHtml').innerHTML = document.getElementById('formHtml').innerHTML + html;
+}
+
+
+function buyClick (){
+    alert("خرید با موفقیت انجام شد.");
+}
